95ffcc57af3554732ec87097cd37510b4a4a32b8
Moonsyn <msy9427@naver.com>
2020-02-04 16:54:32 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+// First, create a file called `test.js`.  For the purposes of this workshop   
+// you can leave it blank, but this would be where you'd write your
+// tests (this is npm class, not testing class).  The test has to exit
+// without throwing an error, or else the test fails.
+
+console.log("Node Test is Successful!")
